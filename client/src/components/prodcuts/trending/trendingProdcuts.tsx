deaf3f34e69a39824337978b41bd936f72f1def6
Mohd Amir <webdev.mohdamir@gmail.com>
2023-12-26 13:17:28 +0530

    added and refine trending prod. section

--- client/src/components/prodcuts/trending/trendingProdcuts.tsx
+++ client/src/components/prodcuts/trending/trendingProdcuts.tsx
@@ -1,14 +1,34 @@
+import { useEffect, useState } from "react";
 import SingleProduct from "../single-product/singleProduct"
 import styles from "./trendingProduct.module.css"
+import { SingleProductType } from "../../../types/product";
+import { dummyProductData } from "../../../constants";
 
 const TrendingProdcuts = () => {
+    const [trendingProd, setTrendingProd] = useState<SingleProductType[] | null>(null);
+
+    useEffect(()=>{
+        const data = dummyProductData;        
+        setTrendingProd(data);
+    }, [])
+
   return (
     <section className={`${styles.container}`}>
         <div className="wrapper">
             <h2>Trending Products</h2>
 
             <div className={styles.products}>
-                <SingleProduct />
+                {trendingProd && trendingProd.map(data => (
+                  <SingleProduct 
+                    prod_id ={data.prod_id}
+                    prod_name ={data.prod_name} 
+                    prod_price ={data.prod_price}
+                    prod_sale ={data.prod_sale} 
+                    prod_tag ={data.prod_tag} 
+                    prod_url ={data.prod_url} 
+                    prod_img_url ={data.prod_img_url}
+                  />
+                ))}
             </div>
         </div>
     </section>
---
+++ client/src/types/product.ts
@@ -0,0 +1,9 @@
+export type SingleProductType ={
+    prod_id : string;
+    prod_name : string,
+    prod_price : number,
+    prod_sale : number,
+    prod_tag : string | null,
+    prod_url : string,
+    prod_img_url : string
+}
