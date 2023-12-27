088ba011058ee07f1d2a7e0cb4fc84d1c7400cec
Mohd Amir <webdev.mohdamir@gmail.com>
2023-12-27 15:33:56 +0530

    Added the banner section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,6 +20,16 @@ function App() {
       </header>
 
       <TrendingProdcuts/>
+
+      {/*  Banner */}
+      <section className='banner'>
+        <div className="banner_info">
+          <h2>We Provide Organic Green Tea For You</h2>
+          <p>Lorem ipsum dolor sit amet, consectetu adipisicing elit sedeiu tempor inci ut labore et dolore magna aliqua.</p>
+          <a href="#" role='button' className='cta_btn'>Read more <span>&gt;&gt;</span></a>
+        </div>
+      </section>
+      {/* END Banner */}
     </>
   )
 }
